6e0b55a51cd82a770957d73f4c86bc4e6c062abb
Krish2208 <diwankrish.17@gmail.com>
2024-09-25 13:24:11 +0530

    labels.idx added

---
+++ code/to_idx_labels.py
@@ -0,0 +1,28 @@
+import numpy as np
+import struct
+
+# Define your labels
+num_labels = 5  # Number of labels/images
+label_class = 0  # Assuming all labels belong to the same class
+
+# Create an array of labels (all the same class)
+labels = np.array([label_class] * num_labels, dtype=np.uint8)
+
+# IDX Header for labels
+# Magic number for labels is 0x00000001
+magic_number = 0x00000001
+num_dims = 1  # Number of dimensions
+labels_shape = labels.shape[0]  # Number of labels
+
+# Create the IDX file
+with open('labels.idx', 'wb') as f:
+    # Write the magic number
+    f.write(struct.pack('>I', magic_number))
+    # Write the number of dimensions
+    f.write(struct.pack('>I', num_dims))
+    # Write the shape of the labels
+    f.write(struct.pack('>I', labels_shape))
+    # Write the actual labels
+    f.write(labels.tobytes())
+
+print("Labels converted to labels.idx")
--- test.py
+++
@@ -1,3 +0,0 @@
-import os
-print(os.path.exists('code/dataset/traffic_light_data/train'))
-print(os.path.exists('code/dataset/traffic_light_data/val'))
